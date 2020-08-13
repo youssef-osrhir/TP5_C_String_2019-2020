754ae89ea2abb3aef69edea185950b5bc0275573
Youssef Osrhir <youssefosrhir@gmail.com>
2020-08-13 16:01:01 +0100

    .\Exo3.c

---
+++ Exo3.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+void supprimer_char(char *s, int i)
+{
+  int j = i;
+  while (s[j] != '\0')
+  {
+    s[j] = s[j + 1];
+    j++;
+  }
+}
+char *supprimer_espaces(char *s)
+{
+  int i = 0;
+  while (s[i] != '\0')
+  {
+    // le cas ou l'espace se trouve à la fin ou au debut
+    if ((i == 0 || s[i + 1] == '\0') && s[i] == ' ')
+      supprimer_char(s, i);
+
+    // le cas ou on a plusieurs espaces suivis au milieu
+    if (s[i] == ' ')
+    {
+      int j = i;
+      while (s[j] == ' ')
+      {
+        supprimer_char(s, j);
+        j++;
+      }
+
+      if (s[j - 1] == ' ')
+        supprimer_char(s, j - 1);
+    }
+
+    i++;
+  }
+  puts(s);
+
+  return s;
+}
+int main()
+{
+  char s[] = " Bonjour      tout      le       monde ";
+  printf("Avant :");
+  puts(s);
+
+  printf("Apres :");
+  supprimer_espaces(s);
+}
---
+++ exo1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+char *reverse(char *s)
+{
+
+    int n = 0;
+
+    while (s[n] != '\0')
+    {
+        n++;
+    }
+
+    //apres while n va contenir la taille
+
+    for (int i = 0; i < n / 2; i++)
+    {
+        char tmp = s[i];
+        s[i] = s[n - i - 1];
+        s[n - i - 1] = tmp;
+    }
+
+    return s;
+}
+
+int main()
+{
+    char c[100];
+
+    gets(c);
+
+    puts(reverse(c));
+}
